e1d34400254b626e4b9084edfe172308da366506
vorstadtcowboy <m.hassenstein@gmx.de>
2022-10-04 15:56:07 +0200

    Todo App mit delete

--- src/App.js
+++ src/App.js
@@ -5,18 +5,58 @@ import { useEffect, useState } from 'react';
 function App() {
   //Den UseState anlegen
   const [todos, setTodos] = useState([]);
+  const [aufgabe, setAufgabe] = useState('');
+  const [id, setId] = useState(0);
 
+  //Use Effect um die Daten zu holen
   useEffect(() => {
     //Listen Daten von der REST Api holen
     axios.get('http://localhost:3004/todos').then((response) => setTodos(response.data));
-  }, []);
+  }, [aufgabe, id]);
 
   //Eine Liste mit React machen
-  const todoListe = todos.map((item) => <li key={item.id}>{item.todo}</li>);
+  const todoListe = todos.map((item) => (
+    <li key={item.id}>
+      {item.todo}{' '}
+      <button id={item.id} onClick={handleDelete}>
+        löschen
+      </button>
+    </li>
+  ));
+
+  //Delete Fuction
+  function handleDelete(event) {
+    axios
+      .delete(`http://localhost:3004/todos/${event.target.id}`)
+      .then(() => setId(event.target.id + 'gelöscht'));
+  }
+
+  //Event Handler funktion für das Submit Event
+  function handleSubmit(event) {
+    event.preventDefault();
+    //Schreibe den Wert des Feldes in die Daten.
+    axios.post('http://localhost:3004/todos', { todo: aufgabe }).then((response) => {
+      //Leert den Wert des Feldes Aufgabe
+      setAufgabe('');
+      console.log(response);
+    });
+  }
 
   return (
     <div className="App">
       <h1>Meine ToDo Liste</h1>
+      {/**Formular zum Todo Eintragen */}
+      <form onSubmit={handleSubmit}>
+        <input
+          required={true}
+          type="text"
+          name="aufgabe"
+          value={aufgabe}
+          onChange={(event) => setAufgabe(event.target.value)}
+        />
+        <button type="submit">senden</button>
+      </form>
+
       <ul>{todoListe}</ul>
     </div>
   );
